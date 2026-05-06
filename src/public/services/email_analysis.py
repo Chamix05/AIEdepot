bb3f92851a0b012d989758640b897b83c12904de
Amina <aminabay005@gmail.com>
2026-05-06 23:02:04 +0200

    Vss

--- src/public/services/email_analysis.py
+++ src/public/services/email_analysis.py
@@ -65,9 +65,12 @@ def safe_encode_sender(sender: str, encoder) -> int:
 
 
 # Marques sensibles et domaines autorisés
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
 
-with open("src/dataset/trusted_domains.json", "r", encoding="utf-8") as f:
+with open(os.path.join(ROOT_DIR, "src/dataset/trusted_domains.json"), "r", encoding="utf-8") as f:
     trusted_domains = json.load(f)
+#with open("src/dataset/trusted_domains.json", "r", encoding="utf-8") as f:
+ #   trusted_domains = json.load(f)
 
 
 #Vérifie si le domaine de l’expéditeur correspond à celui des URLs.
